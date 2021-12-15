a026256d69367309f7a29afb3010764cf1392bb7
sixsalman <58651522+sixsalman@users.noreply.github.com>
2021-12-15 14:53:30 -0500

    Updated config.php

--- php/config.php
+++ php/config.php
@@ -1,5 +1,5 @@
 <?php
 define('DBUSER', '3674097_theclosetdb');
-define('DBPASS', 'wmuCS3500');
+define('DBPASS', '*********');
 define('DBCONNSTRING', 'mysql:host=fdb30.awardspace.net;dbname=3674097_theclosetdb;');
 ?>
